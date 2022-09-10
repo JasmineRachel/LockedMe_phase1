dd2d0ca5cf6e69ce1c24eb2b1f8355455887e531
Jamsine <jasmine.r.davies@gmail.com>
2022-09-10 13:53:25 +0100

    added details for the welcom screen

--- src/com/org/lockedMe/lockedMe.java
+++ src/com/org/lockedMe/lockedMe.java
@@ -1,9 +1,19 @@
 package com.org.lockedMe;
+import java.util.Scanner;
 
 public class lockedMe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to LockedMe.com");
+		System.out.println("Developed by Jasmine Davies");
+		System.out.println("Please select an option");
+		System.out.println("1. View files, 2. add file, 3.add folder 4. delete file, 5. search file, 6. close app");
+		System.out.println("type the number for the option you'd like to action");
+		
+		int option = userInput.nextInt();
+		
 
 	}
 
